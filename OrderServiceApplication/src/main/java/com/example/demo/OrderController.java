de66d49938d2c8b80ad1c84bf9f3ee9f035c58d0
Anand Babu P <anandbabu.x.pancharathinam@kp.org>
2024-12-18 18:40:38 +0530

    initial commit

---
+++ OrderServiceApplication/src/main/java/com/example/demo/OrderController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getOrder(@PathVariable String id) {
+        return ResponseEntity.ok("Order with ID: " + id);
+    }
+}
